a4f0763a2208d6233cb213930efa3174a264bbc8
Aditya S <as2397@hw.ac.uk>
2025-12-23 14:06:39 +0400

    Added robot_description file

---
+++ src/hydrakon_description/launch/display.launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_name = 'hydrakon_description'
+    file_subpath = 'urdf/vehicle.urdf.xacro'
+
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
+    robot_description = Command(['xacro ', xacro_file])
+
+    return LaunchDescription([
+        # 1. Publish the Robot State (TF Tree)
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        
+        # 2. GUI to play with joints (optional, but good for checking wheels later)
+        # Node(
+        #     package='joint_state_publisher_gui',
+        #     executable='joint_state_publisher_gui',
+        #     name='joint_state_publisher_gui'
+        # ),
+
+        # 3. Launch Rviz2
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
